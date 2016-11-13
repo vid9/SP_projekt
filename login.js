36cef88036885b6058c46332c7c1186b8667e6ff
vid9 <vid.cermelj@gmail.com>
2016-11-13 22:13:19 +0100

    added login and register options

--- login.js
+++ login.js
@@ -5,7 +5,12 @@
 function check(form) { /*function to check userid & password*/
     /*the following code checkes whether the entered userid and password are matching*/
     if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd") {
-        window.open('target.html')/*opens the target page while Id & password matches*/
+        if(form.userid.value == "osnovni uporabnik") {
+            window.open('suporabnik.html')/*opens the target page while Id & password matches*/
+        } else {
+            window.open('puporabnik.html')/*opens the target page while Id & password matches*/
+        }
+
     }
     else {
         alert("Error Password or Username")/*displays error message*/
