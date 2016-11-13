f282d58d9e47ad0c1afbb36d77d1dafd88c40be6
vid9 <vid.cermelj@gamil.com>
2016-11-13 23:11:57 +0100

    fixed login, now redirects to page for uporabnik

--- login.js
+++ login.js
@@ -4,15 +4,15 @@
 
 function check(form) { /*function to check userid & password*/
     /*the following code checkes whether the entered userid and password are matching*/
-    if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd") {
-        if(form.userid.value == "osnovni uporabnik") {
-            window.open('suporabnik.html')/*opens the target page while Id & password matches*/
+    if(form.userid.value != ""/* && form.pswrd.value == "test"*/) {
+        if(form.userid.value != "") {/*tip uporabnika osnovni*/
+            window.location.href = "suporabnik.html";/*opens the target page while Id & password matches*/
         } else {
-            window.open('puporabnik.html')/*opens the target page while Id & password matches*/
+            window.open('puporabnik.html','_self')/*opens the target page while Id & password matches*/
         }
-
     }
     else {
         alert("Error Password or Username")/*displays error message*/
     }
+    return true;
 }
--- registration.js
+++ registration.js
@@ -1,3 +1,12 @@
 /**
  * Created by vidce on 13. 11. 2016.
  */
+function add(form) { /*function to add user into system
+    /*the following adds user info into page system*/
+    if(form.userid.value != "current_values") {
+        window.open('index.html',_self)/*opens the target page while Id & password matches*/
+    }
+    else {
+        alert("Error Username")/*displays error message*/
+    }
+}
