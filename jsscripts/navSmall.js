93b7ee8df55e5f6447004fc1db016d4ebfebc0a9
vid9 <vid.cermelj@gamil.com>
2016-11-21 21:34:04 +0100

    updated start page

--- jsscripts/navSmall.js
+++ jsscripts/navSmall.js
@@ -1,7 +1,7 @@
 /**
  * Created by vidce on 20. 11. 2016.
  */
-function myFunction() {
+function smallBar() {
     var x = document.getElementById("navSmall");
     if (x.className.indexOf("show") == -1) {
         x.className += " show";
